91aee1a140ce247f61ec6015135bcaf35ea148b8
parvez mosharrof <pk9435497@gmail.com>
2024-11-05 11:41:41 -0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    
+    './index.html',
+    './bike_list.html',
+    './bike_single.html',
+    './abou_us.html',
+    './contact_us.html',
+    './bike_type.html'
+
+  ],
+  theme: {
+    extend: {
+      colors:{
+        'primary':'#808080',
+        'secondary': '#008000'
+      },
+    
+     
+    },
+  },
+  plugins: [],
+}
+
